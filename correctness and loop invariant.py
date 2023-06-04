cd4853a12e8983b0f76cc534929db610acaf9239
Nidhish Malviya <72844520+nidhish-create@users.noreply.github.com>
2023-06-04 23:55:40 +0530

    Update correctness and loop invariant.py

--- correctness and loop invariant.py
+++ correctness and loop invariant.py
@@ -5,7 +5,7 @@ def insertion_sort(l):
         j = i-1
         key = l[i]
         while (j > 0) and (l[j] > key):
-           l[j+1] = l[j]
+           l[j+1] = l[j]              
            j -= 1
         l[j+1] = key
 
